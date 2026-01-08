fa4add78147c0c7bf5f9bddafd52478cbde500ad
ArcaneLaboratory <joseph.l.mackelprang@gmail.com>
2026-01-08 15:26:52 -0700

    first assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your numeric grade. ");
+        float grade = float.Parse(Console.ReadLine());
+        char letterGrade = ' ';
+        char gradeSign = ' ';
+        if(grade < 80){
+            if(grade < 60) letterGrade = 'F';
+            else if(grade < 70) letterGrade = 'D';
+            else letterGrade = 'C';
+        }else{
+            if(grade < 90) letterGrade = 'B';
+            else letterGrade = 'A';
+        }
+        if(letterGrade != 'F'){
+            if(grade % 10 >= 7 && letterGrade != 'A') gradeSign = '+';
+            else if (grade % 10 < 3) gradeSign = '-';
+        }
+        Console.Write($"Your grade is {letterGrade}{gradeSign}");
     }
 }

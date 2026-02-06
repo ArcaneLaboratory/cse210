ff9ca39be888e2202fbab645e3be75b5ddca6846
ArcaneLaboratory <joseph.l.mackelprang@gmail.com>
2026-02-06 03:52:48 -0700

    Plot sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,41 +1,192 @@
-using System;
-using System.Globalization;
+// Sod Farm Activity
+using System.IO;
+// TODO:
+// 1. Create a Plot class that has the dimensions of a rectangular sod farm plot in feet
+// 2. Complete the TODO in Run
+// 3. Complete the SavePlot() function
+// Stretch Activities
+// 1. Add a name to plot
+// 2. How would the plots be loaded from file?
 
-class Program
+namespace SodFarm
 {
+    // The Plot Class
 
-    static string TitleCase(string input)
+    public class Plot
     {
-        input = input.ToLower();
-        List<int> spaces = [-1];
-        List<string> chars = new();
-        foreach (char item in input)
+        public double length;
+        public double width;
+
+        public string name;
+
+        public Plot()
         {
-            chars.Add(item.ToString());
+            length = 1;
+            width = 1;
+            name = "x";
         }
-        for (int i = 0; i < input.Length - 1; i++)
+
+        public Plot(double dim)
         {
-            if(input[i] == ' ')
+            length = dim;
+            width = dim;
+            name = "x";
+        }
+
+        public Plot(double l, double w)
+        {
+            length = l;
+            width = w;
+            name = "x";
+        }
+        public Plot(string n)
+        {
+            length = 1;
+            width = 1;
+            name = n;
+        }
+
+        public Plot(double dim, string n)
+        {
+            length = dim;
+            width = dim;
+            name = n;
+        }
+
+        public Plot(double l, double w, string n)
+        {
+            length = l;
+            width = w;
+            name = n;
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        override public string ToString()
+        {
+            return $"Plot {name} : {length} by {width}, {GetArea()} ft^2";
+        }
+    }
+
+    class PlotManager
+    {
+        public string _fileName = "sodfarm.txt";
+
+        public List<Plot> _plots = new List<Plot>();
+
+        /// <summary>
+        /// Run the program, collecting dimensions for
+        /// </summary>
+        public void Run()
+        {
+            Console.Write("Load existing plots? (y/n)");
+            if(Console.ReadLine().ToLower() == "y")
             {
-                spaces.Add(i);
+                _plots = LoadPlots();
+                DisplayPlots();
+            }
+            do
+            {
+                Console.Write("Add a plot? (y/n) ");
+
+                if (Console.ReadLine().ToLower() == "n")
+                    break;
+                try
+                {
+                    Console.Write("Enter Length (ft): ");
+                    double l = double.Parse(Console.ReadLine());
+                    Console.Write("Enter Width (ft): ");
+                    double w = double.Parse(Console.ReadLine());
+                    Console.Write("Enter name:");
+                    string n = Console.ReadLine();
+                    n = n.Replace(" ", "_");
+                    _plots.Add(new Plot(l, w, n));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter numeric values.");
+                }
+            } while (true);
+            // Display Plot list
+            DisplayPlots();
+            // Display total area
+            Console.WriteLine($"Total Area is: {CalculateTotalArea()} ft^2");
+            // Save the plots to disk
+            SavePlots();
+        }
+
+        /// <summary>
+        /// Iterate through the plots and calculate the total area
+        /// </summary>
+        /// <returns></returns>
+        public double CalculateTotalArea()
+        {
+            double totalArea = 0;
+
+            foreach (Plot p in _plots)
+            {
+                totalArea += p.GetArea();
+            }
+
+            return totalArea;
+        }
+
+        /// <summary>
+        /// Displays all plots, including dimensions and area
+        /// </summary>
+        public void DisplayPlots()
+        {
+            if (_plots.Count == 0)
+            {
+                Console.WriteLine("No plots found.");
+
+                return;
+            }
+
+            foreach (Plot p in _plots)
+            {
+                Console.WriteLine(p.ToString());
             }
         }
-        foreach(int item in spaces)
-        {
-            chars[item+1] = chars[item+1].ToUpper();
-        }
-        string output = "";
-        foreach (string item in chars)
-        {
-            output += item;
-        }
-        return output;
-    }
 
-    static void Main(String[] args)
+        /// <summary>
+        /// Writes all plots to a file, one plot per line
+        /// Note: _filename is the output file path
+        /// </summary>
+        public void SavePlots()
+        {
+            using (StreamWriter file = new StreamWriter(_fileName))
+            {
+                foreach(Plot p in _plots)
+                {
+                    file.WriteLine(p.ToString());
+                }
+            }
+        }
+        public List<Plot> LoadPlots()
+        {
+            List<Plot> LPlots = new();
+            string[] lines = File.ReadAllLines(_fileName);
+            foreach(string line in lines)
+            {
+                string[] parts = line.Split(" ");
+                string name = parts[1];
+                double length = double.Parse(parts[3]);
+                double width = double.Parse(parts[5]);
+                LPlots.Add(new Plot(length, width, name));
+            }
+            return LPlots;
+        }
+    }
+    class Program
     {
-       Console.WriteLine($"{TitleCase("hello world")}");
+        public static PlotManager pm = new();
+        public static void Main(string[] args)
+        {
+            pm.Run();
+        }
     }
-
-
 }

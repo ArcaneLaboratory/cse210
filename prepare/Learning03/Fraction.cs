ae9a593ed1d9ba1cadb707d621e2b1cab0facddb
ArcaneLaboratory <joseph.l.mackelprang@gmail.com>
2026-02-11 02:01:34 -0700

    Fractions finished

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,25 +1,73 @@
-using System.Numerics;
+using System.Dynamic;
 
 class Fraction
 {
-    private int _n, _d;
+    private int _n,
+        _d;
+
     public Fraction()
     {
         _n = 1;
         _d = 1;
     }
+
+    public Fraction(int whole)
+    {
+        _n = whole;
+        _d = 1;
+    }
+
     public Fraction(int numerator, int denominator)
     {
         _n = numerator;
-        _d = denominator;
+        SetDenominator(denominator);
     }
-    
-    // public int Numerator
-    // {
-    //     get{return _numer;}
-    // }
-    // public int Denominator
-    // {
-    //     get{return _denom;}
-    // }
+
+    public Fraction(Fraction numerator, Fraction denominator)
+    {
+        _n = numerator._n * denominator._d;
+        SetDenominator(numerator._d*denominator._n);
+    }
+
+    public int GetNumerator()
+    {
+        return _n;
+    }
+
+    public int GetDenominator()
+    {
+        return _d;
+    }
+
+    public void SetNumerator(int n)
+    {
+        _n = n;
+    }
+
+    public void SetDenominator(int d)
+    {
+        if (d == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.", nameof(d));
+        }
+        else
+        {
+            _d = d;
+        }
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_n}/{_d}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_n / (double)_d;
+    }
+
+    public override string ToString()
+    {
+        return $"String: {GetFractionString()} Decimal: {GetDecimalValue()}";
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Fraction f = new();
+        Random r = new();
+        int numFractions = 500;
+
+        Fraction f1 = new();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new(2, 5);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new(1, 7);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        Fraction fPrime = new();
+
+        for (int i = 0; i < numFractions; i++)
+        {
+            fPrime.SetNumerator(r.Next(1, 100));
+            fPrime.SetDenominator(r.Next(1, 100));
+            Console.WriteLine($"Fraction {i}: {fPrime}");
+        }
+        {
+            //     Nothing to see here....
+            //
+            //     numFractions = 100000000;
+            //     List<Fraction> BigQ = [new(1, 1)];
+            //     int n = 1;
+            //     int d = 1;
+            //     for (int i = 0; i < numFractions; i++)
+            //     {
+            //         if (n == 1)
+            //         {
+            //             n = d + 1;
+            //             d = 1;
+            //         }
+            //         else
+            //         {
+            //             d++;
+            //             n--;
+            //         }
+            //         BigQ.Add(new(n, d));
+            //     }
+            //     using StreamWriter file = new("BigQ.txt", false);
+            //     foreach (Fraction f in BigQ)
+            //     {
+            //         file.WriteLine(f);
+            //         Console.WriteLine(f);
+            //     }
+            //
+        }
     }
 }

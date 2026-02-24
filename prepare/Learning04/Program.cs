018526f6ebf77cbf438df8967cb07c6a70928fc8
ArcaneLaboratory <joseph.l.mackelprang@gmail.com>
2026-02-24 15:42:20 -0700

    Inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,15 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a = new("Jacob Smith", "Chemistry");
+        WritingAssignment wa = new("Eleanor Jones", "American Heritage", "The Revolutionary War");
+        MathAssignment ma = new("Parker Johnson", "Integration By Parts", "9.1", "1-4");
+
+        Console.WriteLine(a.GetSummary());
+        Console.WriteLine(wa.GetSummary());
+        Console.WriteLine(wa.GetWritingInformation());
+        Console.WriteLine(ma.GetSummary());
+        Console.WriteLine(ma.GetHomeworkList());
     }
 }

10bb3dccc334f70dea3daa4a102d7d69ed364baa
ArcaneLaboratory <joseph.l.mackelprang@gmail.com>
2026-03-10 15:43:33 -0600

    a square is just a rectangle tho

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,11 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = [new Circle("red", 5.0), new Rectangle("blue", 4.5, 8.0), new Square("green", 3.3)];
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()}, {shape.GetArea()}");
+        }
     }
 }
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,7 @@
+public class Square : Rectangle
+{
+    public Square(string color, double side) : base(color, side, side)
+    {
+        // a square is just a rectangle...
+    }
+}

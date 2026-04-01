5aa588d3f67fc31909b81c674d486261f7413795
ArcaneLaboratory <joseph.l.mackelprang@gmail.com>
2026-04-01 02:05:21 -0600

    commented debug line

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -7,7 +7,7 @@ class Program
         List<Card> hand = [
             d.Deal(), d.Deal(), d.Deal(), d.Deal(), d.Deal(), d.Deal(), d.Deal()
         ];
-        hand = [new Card('d', 3), new Card('s', 3), new Card('d', 5), new Card('c', 3), new Card('d', 11), new Card('h', 3), new Card('d', 12)];
+        //hand = [new Card('d', 3), new Card('s', 3), new Card('d', 5), new Card('c', 3), new Card('d', 11), new Card('h', 3), new Card('d', 12)];
         List<Card> bestHand = [];
         foreach(Card c in hand)
         {
